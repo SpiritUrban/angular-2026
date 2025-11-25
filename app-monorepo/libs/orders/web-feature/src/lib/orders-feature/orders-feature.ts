f216e1aad5493908cf53d12aeb2e7cc88460929d
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2025-11-25 02:37:36 +0200

    ui el to page

--- app-monorepo/libs/orders/web-feature/src/lib/orders-feature/orders-feature.ts
+++ app-monorepo/libs/orders/web-feature/src/lib/orders-feature/orders-feature.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { OrdersUi } from '@app-monorepo/orders-ui';
 
 @Component({
   selector: 'lib-orders-feature',
-  imports: [],
+  standalone: true,
+  imports: [OrdersUi],
   templateUrl: './orders-feature.html',
   styleUrl: './orders-feature.css',
 })
